682c5d7396df9577edd81a7827b4eb5f4e051fb5
925634263@qq.com <925634263@qq.com>
2018-12-04 20:55:37 +0800

    Added custom 404 and 500 error pages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect,Http404
+from django.shortcuts import render,get_object_or_404
 # Create your views here.
 def index(request):
     return render(request,'learning_logs/index.html')
@@ -16,7 +17,7 @@ def topics(request):
     return render(request,'learning_logs/topics.html',context)
 @login_required
 def topic(request,topic_id):
-    topic = Topic.objects.get(id = topic_id)
+    topic = get_object_or_404(Topic,id = topic_id)
     if topic.owner != request.user:
         raise Http404
     entries = topic.entry_set.order_by('-date_added')
